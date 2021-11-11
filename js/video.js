50f9963a0c4e52cfc4c814ae2dd5d65d250fd7e3
jensieg <jensieg@umich.edu>
2021-11-10 21:06:13 -0500

    pause and play buttons

--- js/video.js
+++ js/video.js
@@ -5,7 +5,13 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+	video.play()
+	document.querySelector('#volume').innerHTML=video.volume * 100 + "%"
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+});
